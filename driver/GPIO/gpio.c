b7cded6cf46253d9773d5f439d57d03abc725262
Nhi Van Pham <nhivp@gcs-vn.com>
2018-08-02 14:11:15 +0700

    GPIO: Update source code

--- driver/GPIO/gpio.c
+++ driver/GPIO/gpio.c
@@ -1,39 +1,115 @@
 /*
- * gpio.c
- * Implementation of GPIO Driver
+ * File: gpio.c
+ * Description: Implementation of GPIO Driver
  *
  * Created by nhivp on 23/07/2018.
  * Copyright (C) 2018 nhivp. All rights reserved.
  */
-
+/*****************************************************************************
+ * Includes <Header Files>
+ *****************************************************************************/
 #include "gpio.h"
 
-uint8_t GPIO_Initialize(GPIO_Port_t aPort, GPIO_Direction_t aDir);
-{
+/*****************************************************************************
+ * Macro definitions
+ *****************************************************************************/
 
+/*****************************************************************************
+ * Private typedef definitions
+ *****************************************************************************/
+typedef struct
+{
+    volatile unsigned char DIR;
+    volatile unsigned char IN;
+    volatile unsigned char OUT;
+    volatile unsigned char REN;
+    volatile unsigned char SEL;
+    volatile unsigned char SEL2;
+    volatile unsigned char IES;
+    volatile unsigned char IE;
+    volatile unsigned char IFG;
+    GPIO_Callback_t GPIO_Callback_Func[8];
+} GPIO_Management_t;
+
+/*****************************************************************************
+ * Private global variables
+ *****************************************************************************/
+static GPIO_Management_t GPIO_Mng_Tbl
+{
+    {P1DIR, P1IN, P1OUT, P1REN, P1SEL, P1SEL2, P1IES, P1IE, P1IFG, {0, 0, 0, 0, 0, 0, 0, 0}}, /* Port 1 */
+    {P2DIR, P2IN, P2OUT, P2REN, P2SEL, P2SEL2, P2IES, P2IE, P2IFG, {0, 0, 0, 0, 0, 0, 0, 0}}  /* Port 2 */
+};
+
+/*****************************************************************************
+ * Private global functions
+ *****************************************************************************/
+
+/*****************************************************************************
+ * Public global functions
+ *****************************************************************************/
+void GPIO_Initialize(GPIO_Pin_t aPin, GPIO_Direction_t aDir)
+{
+    GPIO_Mng_Tbl[aPin / GPIO_PIN_NUM].DIR |= (aDir << (aPin % GPIO_PIN_NUM));
 }
 
-uint8_t GPIO_ConfigInterrupt(GPIO_Port_t aPort, GPIO_Edge_t aEdge, GPIO_Callback_t aCallBackFunc)
+void GPIO_ConfigInterrupt(GPIO_Pin_t aPin, GPIO_Edge_t aEdge, GPIO_Callback_t aCallBackFunc)
 {
-
+    GPIO_Mng_Tbl[aPin / GPIO_PIN_NUM].GPIO_Callback_Func[aPin % GPIO_PIN_NUM] = aCallBackFunc;
+    GPIO_Mng_Tbl[aPin / GPIO_PIN_NUM].IES |= (aEdge << (aPin % GPIO_PIN_NUM));
 }
 
-bool    GPIO_ReadPin(GPIO_Port_t aPort)
+bool GPIO_ReadPin(GPIO_Pin_t aPin)
 {
-
+    return (GPIO_Mng_Tbl[aPin / GPIO_PIN_NUM].OUT & (aPin % GPIO_PIN_NUM) > 0);
 }
 
-uint8_t GPIO_ReadPort(GPIO_Port_t aPort)
+uint8_t GPIO_ReadPort(GPIO_Pin_t aPin)
 {
-
+    return GPIO_Mng_Tbl[aPin / GPIO_PIN_NUM].OUT;
 }
 
-uint8_t GPIO_WritePin(GPIO_Port_t aPort, bool aValue)
+void GPIO_WritePin(GPIO_Pin_t aPin, bool aValue)
 {
-
+    if (aValue)
+    {
+        GPIO_Mng_Tbl[aPin / GPIO_PIN_NUM].IN |= (1 << (aPin % GPIO_PIN_NUM));
+    }
+    else
+    {
+        GPIO_Mng_Tbl[aPin / GPIO_PIN_NUM].IN |= (0 << (aPin % GPIO_PIN_NUM));
+    }
 }
 
-uint8_t GPIO_WritePort(GPIO_Port_t aPort, uint8_t aValue)
+void GPIO_WritePort(GPIO_Pin_t aPin, uint8_t aValue)
 {
-
+    GPIO_Mng_Tbl[aPin / GPIO_PIN_NUM].IN |= aValue;
 }
+
+void GPIO_SelectFunc(GPIO_Pin_t aPin, GPIO_FunctionSel_t aFuncSel)
+{
+    switch (aFuncSel)
+    {
+        case GPIO_IO_FUNC:
+            GPIO_Mng_Tbl[aPin / GPIO_PIN_NUM].SEL  |= (0 << (aPin % GPIO_PIN_NUM));
+            GPIO_Mng_Tbl[aPin / GPIO_PIN_NUM].SEL2 |= (0 << (aPin % GPIO_PIN_NUM));
+            break;
+
+        case GPIO_PRIMARY_FUNC:
+            GPIO_Mng_Tbl[aPin / GPIO_PIN_NUM].SEL  |= (1 << (aPin % GPIO_PIN_NUM));
+            GPIO_Mng_Tbl[aPin / GPIO_PIN_NUM].SEL2 |= (0 << (aPin % GPIO_PIN_NUM));
+            break;
+
+        case GPIO_SECONDARY_FUNC:
+            GPIO_Mng_Tbl[aPin / GPIO_PIN_NUM].SEL  |= (1 << (aPin % GPIO_PIN_NUM));
+            GPIO_Mng_Tbl[aPin / GPIO_PIN_NUM].SEL2 |= (0 << (aPin % GPIO_PIN_NUM));
+            break;
+
+        default:
+            /* Error handler */
+            break;
+    }
+}
+
+/*****************************************************************************
+ * End of File.
+ *****************************************************************************/
--- driver/GPIO/gpio.h
+++ driver/GPIO/gpio.h
@@ -1,46 +1,71 @@
+/*****************************************************************************
+ * Includes <Header Files>
+ *****************************************************************************/
+#include "drv.h"
 
-#include <stdint.h>
+/*****************************************************************************
+ * Macro definitions
+ *****************************************************************************/
+#define GPIO_PORT_NUM    2
+#define GPIO_PIN_NUM     8
 
+/*****************************************************************************
+ * Typedef definitions
+ *****************************************************************************/
 typedef enum
 {
-    PORT_1 = 0,
-    PORT_2
+    /* Pin P1.x */
+    GPIO_PIN_10 = 0,  /* Pin 0 */
+    GPIO_PIN_11,      /* Pin 1 */
+    GPIO_PIN_12,      /* Pin 2 */
+    GPIO_PIN_13,      /* Pin 3 */
+    GPIO_PIN_14,      /* Pin 4 */
+    GPIO_PIN_15,      /* Pin 5 */
+    GPIO_PIN_16,      /* Pin 6 */
+    GPIO_PIN_17,      /* Pin 7 */
 
-} GPIO_Port_t;
-
-typedef enum
-{
-    PIN_0 = 0,
-    PIN_1,
-    PIN_2,
-    PIN_3,
-    PIN_4,
-    PIN_5,
-    PIN_6,
-    PIN_7
+    /* Pin P2.x */
+    GPIO_PIN_20,      /* Pin 0 */
+    GPIO_PIN_21,      /* Pin 1 */
+    GPIO_PIN_22,      /* Pin 2 */
+    GPIO_PIN_23,      /* Pin 3 */
+    GPIO_PIN_24,      /* Pin 4 */
+    GPIO_PIN_25,      /* Pin 5 */
+    GPIO_PIN_26,      /* Pin 6 */
+    GPIO_PIN_27       /* Pin 7 */
 } GPIO_Pin_t;
 
-typedef struct
-{
-    GPIO_Port_t    Port;
-    GPIO_Pin_t     Pin;
-} GPIO_t;
-
 typedef enum
 {
-
+    GPIO_DIR_IN = 0, /* The port pin is switched to input direction */
+    GPIO_DIR_OUT     /* The port pin is switched to output direction */
 } GPIO_Direction_t;
 
 typedef enum
 {
-
+    GPIO_EDGE_RISE = 0, /* The PxIFGx flag is set with a low-to-high transition */
+    GPIO_EDGE_FALL      /* The PxIFGx flag is set with a hogh-to-low transition */
 } GPIO_Edge_t;
 
-typedef void (*GPIO_Callback_t)(GPIO_Port_t aPort, uint8_t aResult);
+typedef enum
+{
+    GPIO_IO_FUNC = 0,   /* I/O function is selected. */
+    GPIO_PRIMARY_FUNC,  /* Primary peripheral module function is selected. */
+    GPIO_SECONDARY_FUNC /* Secondary peripheral module function is selected. */
+} GPIO_FunctionSel_t;
 
-extern uint8_t GPIO_Initialize(GPIO_Port_t aPort, GPIO_Direction_t aDir);
-extern uint8_t GPIO_ConfigInterrupt(GPIO_Port_t aPort, GPIO_Edge_t aEdge, GPIO_Callback_t aCallBackFunc);
-extern bool    GPIO_ReadPin(GPIO_Port_t aPort);
-extern uint8_t GPIO_ReadPort(GPIO_Port_t aPort);
-extern uint8_t GPIO_WritePin(GPIO_Port_t aPort, bool aValue);
-extern uint8_t GPIO_WritePort(GPIO_Port_t aPort, uint8_t aValue);
+typedef void (*GPIO_Callback_t)(GPIO_Pin_t aPin, uint8_t aResult);
+
+/*****************************************************************************
+ * Function prototypes
+ *****************************************************************************/
+extern void    GPIO_Initialize(GPIO_Pin_t aPin, GPIO_Direction_t aDir);
+extern void    GPIO_ConfigInterrupt(GPIO_Pin_t aPin, GPIO_Edge_t aEdge, GPIO_Callback_t aCallBackFunc);
+extern bool    GPIO_ReadPin(GPIO_Pin_t aPin);
+extern uint8_t GPIO_ReadPort(GPIO_Pin_t aPin);
+extern void    GPIO_WritePin(GPIO_Pin_t aPin, bool aValue);
+extern void    GPIO_WritePort(GPIO_Pin_t aPin, uint8_t aValue);
+extern void    GPIO_SelectFunc(GPIO_Pin_t aPin, GPIO_FunctionSel_t aFuncSel);
+/*****************************************************************************
+ * End of File.
+ *****************************************************************************/
